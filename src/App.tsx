b977a78a74619c267af29bceea80d8a07ef8c8ee
siam114 <smsiamislam598@gmail.com>
2026-09-17 21:44:38 +0600

    define type file and get promise

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import './App.css'
+import type { CountryType } from './type';
 
 function App() {
+
+  //step 1: create a promise function to fetch data from the api
+  const countriesPromise = async ():Promise<CountryType[]>=>{
+    const res = await fetch('https://openapi.programming-hero.com/api/all')
+    const data = await res.json()
+    return data.countries;
+  }
 
   return (
     <>
